cfb9819645e3419b2cb38dcdb5333d877e55d66a
Taylor Quinn <ktquinn13@gmail.com>
2016-10-25 13:33:31 -0400

    changed the base url

--- app/config.php
+++ app/config.php
@@ -2,7 +2,7 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://localhost/cs3744/project3/taylorq');
+define('BASE_URL','http://ec2-35-161-94-44.us-west-2.compute.amazonaws.com/');
 
 // database constants
 define('DB_HOST','127.0.0.1');
