dc92f6ab15506aa06b74355382041065b8a2b989
Sean Crenshaw <seanpc9@vt.edu>
2016-10-28 12:01:35 -0400

    Fix return to home

--- app/controller/return.php
+++ app/controller/return.php
@@ -147,7 +147,7 @@
     			<p>Currency Code: <?php  echo($currencyCode);?> </p>
     			<p>Payment Status: <?php  echo($paymentStatus);?> </p>
     			<p>Payment Type: <?php  echo($paymentType);?> </p>
-    			<h3> Click <a href='<?= BASE_URL ?>'>here </a> to return to Home Page</h3>
+    			<h3> Click <a href='/cs3744/Splattered/'>here </a> to return to Home Page</h3>
     			</div>
     		</span>
     		<span class="span3">
