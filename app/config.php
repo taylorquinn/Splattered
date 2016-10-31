17ba331b4805ed08ff1c74dac0be9b85a5506f67
Taylor Quinn <ktquinn13@gmail.com>
2016-10-31 12:13:16 -0400

    This is the correct one!! Don't upload changes to the config

--- app/config.php
+++ app/config.php
@@ -2,7 +2,7 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','splattered.me/Splattered');
+define('BASE_URL','http://ec2-35-161-14-253.us-west-2.compute.amazonaws.com/Splattered/');
 
 // database constants
 define('DB_HOST','127.0.0.1');
