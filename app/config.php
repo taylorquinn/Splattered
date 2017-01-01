e34638b97aaaa1d8c94d8b869469d504b05320e5
Taylor Quinn <ktquinn13@gmail.com>
2017-01-01 13:11:11 -0500

    changed .config

--- app/config.php
+++ app/config.php
@@ -2,7 +2,7 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://splattered.me/ktaylorq');
+define('BASE_URL','http://splattered.me/ktaylorquinn/');
 
 // database constants
 define('DB_HOST','127.0.0.1');
