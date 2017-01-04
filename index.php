5d7ed40a5113336e86e6aafb6539bc4f8ba1e15a
Taylor Quinn <ktquinn13@gmail.com>
2017-01-04 15:38:11 -0500

    updates for title

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html >
 <html xmlns="http://www.w3.org/1999/xhtml"
-      xmlns:fb="http://ogp.me/ns/fb#">>
- <meta http-equiv="refresh" content="0; URL='http://splattered.me/ktq'" />
- <meta property="og:image" content="http://splattered.me/ktq/public/img/girlonmountain.jpg" />
+      xmlns:fb="http://ogp.me/ns/fb#">
+   <head>
+ 	
+ 	<meta property="og:image" content="http://splattered.me/ktq/public/img/girlonmountain.jpg" />
 	<meta charset="utf-8">
    <meta property="og:description" content="This is the personal/ professional site of Taylor Quinn">
    <meta property="og:url" content="http://www.splattered.me" />
 	<meta property="og:title" content="K Taylor Quinn" />
 
+	</head>
+<meta http-equiv="refresh" content="0; URL='http://splattered.me/ktq'" />
 
 </html>
